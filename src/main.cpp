f696265cfc1bda630738503d15733f0e7a11b490
Singu99 <marc.espuna@salle.url.edu>
2024-04-18 19:28:41 +0200

    Added libs so it compiles :)

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,14 @@
 #include <WiFi.h>
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(17, OUTPUT); 
-  // pinMode(13, ANALOG);
-  WiFi.softAP("ESP32-AP", "123456789");
-
-  // Print the IP address with printf
-  printf("IP address: %s\n", WiFi.softAPIP().toString().c_str());
+  
 }
 
 
 void loop() {
-  // Toggle external led (pin 17)
-  digitalWrite(LED_BUILTIN, HIGH);
+
+
+
+
+
 }
